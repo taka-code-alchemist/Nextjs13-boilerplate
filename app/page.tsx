1091f8dd22779ed42c44438ccd3b9fcb48741b49
taka <taka@guis.jp>
2023-03-23 16:36:38 +0900

    tailwindcss導入

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <h1>
-        Hello World!
-      </h1>
+      <h1 className='text-9xl'>Hello World!</h1>
+      <h2 className='text-9xl'>Good by my earth...</h2>
     </main>
   )
 }
